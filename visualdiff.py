9bc272d6cc85ddd2a71ce151c29c59ea4a4b28c9
Carl Maxwell <carl.maxwell@gmail.com>
2015-07-11 21:47:03 -0700

    clear out any changes in the tree before pull

--- visualdiff.py
+++ visualdiff.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python 
+#! /usr/bin/env python
 
 import argparse
 
@@ -12,9 +12,10 @@ name = working.split("/")[-1]
 
 tree = "/".join(working.split("/")[0:-1]) + "/trees/" + name
 
+print("clearing out any changes in tree")
+system("git --git-dir=" + tree + "/.git checkout .")
 print("updating tree")
 system("git --git-dir=" + tree + "/.git pull")
 
 print("calling meld")
 system("meld " + tree + " " + working)
-
