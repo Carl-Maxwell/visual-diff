f61a90bd7d20be67dea0b1895b048042f7ce96d7
Carl Maxwell <carl.maxwell@gmail.com>
2015-07-11 23:08:16 -0700

    check that repo exists, do not call checkout .
    Apparently checkout ignores the --git-dir option,
    and I can find no way to run checkout/reset/etc
    from outside the repo's folder.

--- visualdiff.py
+++ visualdiff.py
@@ -12,10 +12,19 @@ name = working.split("/")[-1]
 
 tree = "/".join(working.split("/")[0:-1]) + "/trees/" + name
 
-print("clearing out any changes in tree")
-system("git --git-dir=" + tree + "/.git checkout .")
+if not path.exists(tree + "/.git"):
+    raise Exception(".git file does not exist in tree! " + tree)
+
+#print("clearing out any changes in tree")
+#print "git --git-dir=" + tree + "/.git checkout ."
+#system("git --git-dir=" + tree + "/.git checkout .")
 print("updating tree")
+print "git --git-dir=" + tree + "/.git pull"
 system("git --git-dir=" + tree + "/.git pull")
 
 print("calling meld")
+print "meld " + tree + " " + working
 system("meld " + tree + " " + working)
+
+
+# TODO: add a verbose option to disable printing of commands that are run
