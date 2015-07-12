82e3e59afa009403f1492becbdcaf5ce50ac2457
Carl Maxwell <carl.maxwell@gmail.com>
2015-07-11 20:45:00 -0700

    initial commit

---
+++ visualdiff.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python 
+
+import argparse
+
+from os import path, system
+
+parser = argparse.ArgumentParser(description='Use meld to visually see changes between your working branch and github git repositories')
+
+working = path.realpath("./")
+
+name = working.split("/")[-1]
+
+tree = "/".join(working.split("/")[0:-1]) + "/trees/" + name
+
+print("updating tree")
+system("git --git-dir=" + tree + "/.git pull")
+
+print("calling meld")
+system("meld " + tree + " " + working)
+
